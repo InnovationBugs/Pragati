a7f2151264cb14aca8835f5e77ac026f9cc4c104
Subhajit Mondal <Subhajit25Mondal@users.noreply.github.com>
2018-07-07 17:24:11 +0530

    firebase files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { VolLoginPage } from '../pages/vol-login/vol-login';
+import { VolSignupPage } from '../pages/vol-signup/vol-signup';
+import { DonatePage } from '../pages/donate/donate';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { NeedFoodPage } from '../pages/need-food/need-food'
+import { ExtraFoodInfoPage } from '../pages/extra-food-info/extra-food-info';
+import { DonateItemPostedPage } from '../pages/donate-item-posted/donate-item-posted';
+import { TestimonalsPage } from '../pages/testimonals/testimonals';
+import { AddCommentPage } from '../pages/add-comment/add-comment';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2'
+import { FIREBASE_CONFIG } from './app.firebase.config';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
@@ -17,11 +32,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    VolLoginPage,
+    VolSignupPage,
+    DonatePage,
+    DashboardPage,
+    NeedFoodPage,
+    ExtraFoodInfoPage,
+    DonateItemPostedPage,
+    TestimonalsPage,
+    AddCommentPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +56,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    VolLoginPage,
+    VolSignupPage,
+    DonatePage,
+    DashboardPage,
+    NeedFoodPage,
+    ExtraFoodInfoPage,
+    DonateItemPostedPage,
+    TestimonalsPage,
+    AddCommentPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    InAppBrowser
   ]
 })
 export class AppModule {}
